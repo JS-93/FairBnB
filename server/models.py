10fb36b293b8d82f81f0faec195a3bf55b53bd17
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-10-16 14:56:36 -0700

    Update models.py
    got rid of models

--- server/models.py
+++ server/models.py
@@ -6,11 +6,6 @@ from config import db, bcrypt
 # Models go here!
 
 
-wishlist_table = db.Table('wishlist', db.Column(
-    'user_id', db.Integer, db.ForeignKey('users.id'), primary_key = True
-), db.Column(
-    'rental_id', db.Integer, db.ForeignKey('rentals.id'), primary_key = True
-))
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
@@ -18,12 +13,12 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique = True, nullable = False)
     _password_hash = db.Column(db.String)
-    reviews = db.relationship('Review', backref='user', cascade='all, delete-orphan')
     bookings = db.relationship('Booking', backref='user', cascade='all, delete-orphan')
-    wishlist = db.relationship('Rental', secondary=wishlist_table, back_populates='wishlist_users')
+   
 
 
-    serialize_rules=('-bookings.user', '-reviews.user', '-wishlist',)
+    serialize_rules=()
+    
 
     @hybrid_property
     def password(self):
@@ -41,20 +36,7 @@ class User(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<User {self.username}, ID {self.id}>'
     
-    
-class Review(db.Model, SerializerMixin):
-    __tablename__ = 'reviews'
 
-    id = db.Column(db.Integer, primary_key = True)
-    rating = db.Column(db.Integer)
-    comment = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    rental_id = db.Column(db.Integer, db.ForeignKey('rentals.id'))
-
-    serialize_rules = ('-user.reviews', '-rental.reviews',)
-
-    def __repr__(self):
-        return f'<Review {self.user_id}, {self.rental_id}, {self.rating}>'
 
 class Rental(db.Model, SerializerMixin):
     __tablename__ = 'rentals'
@@ -65,11 +47,7 @@ class Rental(db.Model, SerializerMixin):
     price = db.Column(db.Integer)
     description = db.Column(db.String)
     image = db.Column(db.String)
-    reviews = db.relationship('Review', backref='rental', cascade='all, delete-orphan')
     bookings = db.relationship('Booking', backref='rental', cascade='all, delete-orphan')
-    wishlist_users = db.relationship('User', secondary=wishlist_table, back_populates='wishlist')
-
-    serialize_rules=('-bookings.rental', '-reviews.rental', 'wishlist_users',)
 
     def __repr__(self):
         return f'<Rental {self.name}, ${self.price}/per night, {self.location}>'
@@ -83,8 +61,8 @@ class Booking(db.Model, SerializerMixin):
     start_date = db.Column(db.DateTime)
     end_date = db.Column(db.DateTime)
 
-    serialize_rules = ('-user.bookings', '-rental.bookings',)
 
     def __repr__(self):
         return f'<Booking {self.id}, {self.start_date}, {self.end_date}>'
+
 
