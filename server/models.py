bf803f0f78ce6df0cee9d01e610c5b8477698e45
Joseph <schnebly.joseph@gmail.com>
2023-10-15 17:20:13 -0700

    created db

--- server/models.py
+++ server/models.py
@@ -1,6 +1,75 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.ext.hybrid import hybrid_property
 from config import db
 
 # Models go here!
+
+
+wishlist_table = db.Table('wishlist', db.Column(
+    'user_id', db.Integer, db.ForeignKey('users.id'), primary_key = True
+), db.Column(
+    'rental_id', db.Integer, db.ForeignKey('rentals.id'), primary_key = True
+))
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    password = db.Column(db.String, nullable = False)
+    reviews = db.relationship('Review', backref='user', cascade='all, delete-orphan')
+    bookings = db.relationship('Booking', backref='user', cascade='all, delete-orphan')
+    wishlist = db.relationship('Rental', secondary=wishlist_table, back_populates='wishlist_users')
+    serialize_rules=('-bookings.user', '-reviews.user',)
+
+    def __repr__(self):
+        return f'<User {self.username}, ID {self.id}>'
+    
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key = True)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    rental_id = db.Column(db.Integer, db.ForeignKey('rentals.id'))
+
+    serialize_rules = ('-user.reviews', '-rental.reviews',)
+
+    def __repr__(self):
+        return f'<Review {self.user_id}, {self.rental_id}, {self.rating}>'
+
+class Rental(db.Model, SerializerMixin):
+    __tablename__ = 'rentals'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, unique = True, nullable = False)
+    location = db.Column(db.String, nullable = False)
+    price = db.Column(db.Integer)
+    description = db.Column(db.String)
+    image = db.Column(db.String)
+    reviews = db.relationship('Review', backref='rental', cascade='all, delete-orphan')
+    bookings = db.relationship('Booking', backref='rental', cascade='all, delete-orphan')
+    wishlist_users = db.relationship('User', secondary=wishlist_table, back_populates='wishlist')
+
+    serialize_rules=('-bookings.rental', '-reviews.rental',)
+
+    def __repr__(self):
+        return f'<Rental {self.name}, ${self.price}/per night, {self.location}>'
+
+class Booking(db.Model, SerializerMixin):
+    __tablename__ = 'bookings'
+
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    rental_id = db.Column(db.Integer, db.ForeignKey('rentals.id'))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+
+    serialize_rules = ('-user.bookings', '-rental.bookings',)
+
+    def __repr__(self):
+        return f'<Booking {self.id}, {self.start_date}, {self.end_date}>'
+
--- server/seed.py
+++ server/seed.py
@@ -2,16 +2,60 @@
 
 # Standard library imports
 from random import randint, choice as rc
+from datetime import datetime
 
 # Remote library imports
-from faker import Faker
+
 
 # Local imports
 from app import app
-from models import db
+from models import Booking, Review, wishlist_table, Rental, User, db
+def seed():
+    db.session.query(Booking).delete()
+    db.session.query(Review).delete()
+    db.session.query(wishlist_table).delete()
+    db.session.query(Rental).delete()
+    db.session.query(User).delete()
+    db.session.commit()
+
+    user1 = User(username= 'Bob', password='Doggo')
+    user2 = User(username="Jimmy", password='Catto')
+    user3 = User(username='Amy', password='Birdo')
+
+    db.session.add_all([user1, user2, user3])
+    db.session.commit()
+
+    rental1 = Rental(name="Bear Cove", location="Country Side", price=150, description="Quiet")
+    rental2 = Rental(name="Tiger Ridge", location="Mountains", price=200, description="High up")
+    rental3 = Rental(name="Waterside Lodge", location="Ocean Front", price=120, description="Waterfront")
+
+    db.session.add_all([rental1, rental2, rental3])
+    db.session.commit()
+
+    user1.wishlist.append(rental2)
+    user2.wishlist.append(rental3)
+    user3.wishlist.append(rental1)
+
+    booking1 = Booking(user_id=user1.id, rental_id=rental3.id, start_date=datetime(2023, 10, 20), end_date=datetime(2023, 10, 23))
+    
+    db.session.add(booking1)
+    db.session.commit()
+
+
+
+
+    review1 = Review(rating=4, comment="Very accommodating!", user_id=user1.id, rental_id=rental1.id)
+    review2 = Review(rating=2, comment="Could be much better..", user_id=user2.id, rental_id=rental2.id)
+
+    db.session.add_all([review1, review2])
+
+    db.session.commit()
+
+
 
 if __name__ == '__main__':
-    fake = Faker()
+    
     with app.app_context():
         print("Starting seed...")
+        seed()
         # Seed code goes here!
